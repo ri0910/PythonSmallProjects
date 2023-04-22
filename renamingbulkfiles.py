1975a8255f16fa7fe392e5a41ece8e82c599645c
Riya karmakar <riyakarmakar.kolkata@gmail.com>
2023-04-22 20:27:57 +0530

    Two more small projects

---
+++ renamingbulkfiles.py
@@ -0,0 +1,15 @@
+import os
+
+
+def main():
+    i = 0
+    path = "C:/Users/riya/Documents/test/"
+    for filename in os.listDir(path):
+        my_dest =  "img"+ str(i) + ".jpg"
+        my_source =path + filename
+        my_dest =path + my_dest
+        os.rename(my_source, my_dest)
+        i += 1
+
+if __name__ == "__main__":
+    main()
